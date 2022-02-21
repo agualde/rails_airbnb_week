15f53ec2329a2ce03fee0bb3b2c61cab9cf3675f
Andres Gualde <andres@gualde.com>
2022-02-21 17:01:54 +0100

    Basic layout of migrations and devise

---
+++ db/migrate/20220221153836_create_flats.rb
@@ -0,0 +1,15 @@
+class CreateFlats < ActiveRecord::Migration[6.1]
+  def change
+    create_table :flats do |t|
+      t.string :title
+      t.string :description
+      t.string :images
+      t.string :address
+      t.integer :capacity
+      t.integer :price
+      t.references :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_21_153957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "users_id", null: false
+    t.bigint "flats_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flats_id"], name: "index_bookings_on_flats_id"
+    t.index ["users_id"], name: "index_bookings_on_users_id"
+  end
+
+  create_table "flats", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "images"
+    t.string "address"
+    t.integer "capacity"
+    t.integer "price"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_flats_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "profile_picture_url"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "flats", column: "flats_id"
+  add_foreign_key "bookings", "users", column: "users_id"
+  add_foreign_key "flats", "users", column: "users_id"
 end
