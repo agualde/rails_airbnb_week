8c664c7893e50c2d0eb88829d0a943284a510713
Guillem Roma <guillemroma.lopez@gmail.com>
2022-02-22 22:39:01 +0100

    Homepage and dashboard improvements (carousel and cards)

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,5 +5,7 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
 
 export { application }
