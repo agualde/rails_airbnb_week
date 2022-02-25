70009aae8c18af355759956ad649ec72e604372b
Andres Gualde <andres@gualde.com>
2022-02-25 23:47:25 +0100

    Added favorites boolean column to Bookings, defaults to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_25_125005) do
+ActiveRecord::Schema.define(version: 2022_02_25_224608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_02_25_125005) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "guests", default: 0
+    t.boolean "favorite", default: false
     t.index ["flat_id"], name: "index_bookings_on_flat_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -65,8 +66,8 @@ ActiveRecord::Schema.define(version: 2022_02_25_125005) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude", default: false
-    t.float "longitude", default: false
+    t.float "latitude", default: 0.0
+    t.float "longitude", default: 0.0
     t.boolean "accept_pets", default: false
     t.boolean "air_con", default: false
     t.boolean "kitchen", default: false
@@ -80,6 +81,7 @@ ActiveRecord::Schema.define(version: 2022_02_25_125005) do
     t.boolean "balcony", default: false
     t.integer "rooms", default: 1
     t.string "location"
+    t.boolean "favorited", default: false
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
